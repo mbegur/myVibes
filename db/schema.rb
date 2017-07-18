54cc12276a2c73a8d6b66e4ba3e7768bd16cc502
Mallik Begur <mbegur@gmail.com>
2017-07-18 11:08:27 -0700

    Start session components

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718044353) do
+ActiveRecord::Schema.define(version: 20170718173851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170718044353) do
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "profile_pic_url"
     t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
---
+++ frontend/components/session/session_form_container.js
@@ -0,0 +1,3 @@
+import { connect } from 'react_redux';
+import { signup, login, logout } from '../../actions/auth_actions';
+import SessionForm from './session_form';
