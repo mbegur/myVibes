a9e1da38677beffd0f713cbb5e8d3f75041b51a7
Mallik Begur <mbegur@gmail.com>
2017-07-21 17:10:50 -0700

    aws fix

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module SoundCloudClone
     # -- all .rb files in that directory are automatically loaded.
 
     config.paperclip_defaults = {
+      :s3_host_name => "s3-#{ENV["s3-region"]}.amazonaws.com",
       :storage => :s3,
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
