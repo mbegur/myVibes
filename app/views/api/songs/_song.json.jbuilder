9b26bd499aef13688f73f83c87a29dedeffa76e0
Mallik Begur <mbegur@gmail.com>
2017-07-24 00:41:09 -0700

    Add player functionality

--- app/views/api/songs/_song.json.jbuilder
+++ app/views/api/songs/_song.json.jbuilder
@@ -3,5 +3,5 @@ json.image_file_name song.image.url
 json.song_file_name song.track.url
 
 json.user do
-  json.extract! song.user, :username
+  json.extract! song.user, :username, :id
 end
--- frontend/components/songs/song_index_item.jsx
+++ frontend/components/songs/song_index_item.jsx
@@ -21,16 +21,22 @@ class SongIndexItem extends React.Component {
 
     return (
       <div className='overall-song-index-item'>
-        <Link className="link-to_song" to={`/songs/${song.id}`}><li className="song-index-item">
+        <li className="song-index-item">
           <Link to={`/songs/${song.id}`}>
             <img className="song-index-image" height="180" width="180" src={song.image_file_name} alt="cover-photo" />
           </Link>
-          <ul>
-            <li className="song-title">{ song.title }</li>
-            <li className="song-username">{ song.user.username }</li>
-            <li><button className="play-button" onClick={this.playTheSong(song)}><img src="http://res.cloudinary.com/mbegur/image/upload/v1500875354/if_button_green_play_50029_klsfgj.png" alt="Play_Button" height="42" width="42"></img></button></li>
-          </ul>
-        </li></Link>
+          <div>
+            <button className="play-button" onClick={this.playTheSong(song)}><img src="http://res.cloudinary.com/mbegur/image/upload/v1500875354/if_button_green_play_50029_klsfgj.png" alt="Play_Button" height="42" width="42"></img></button>
+          </div>
+          <div>
+            <ul>
+              <Link className="link-to_song" to={`/songs/${song.id}`}><li className="song-title">{ song.title }</li></Link>
+              <Link to={`/users/${song.user.id}`}><li className="song-username">{ song.user.username }</li></Link>
+            </ul>
+          </div>
+
+
+        </li>
       </div>
 
     );
