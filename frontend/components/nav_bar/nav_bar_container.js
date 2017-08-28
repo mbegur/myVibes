fa9c59e71295ef519651610ef30ca32eaafa949a
Mallik Begur <mbegur@gmail.com>
2017-08-28 01:04:18 -0700

    Add search reducer and action to container

--- frontend/components/nav_bar/nav_bar_container.js
+++ frontend/components/nav_bar/nav_bar_container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { signup, login, logout } from '../../actions/auth_actions';
+import { receiveSearch } from '../../actions/search_actions';
 import NavBar from './nav_bar';
 
 const mapStateToProps = ({ session }) => ({
@@ -9,6 +10,7 @@ const mapStateToProps = ({ session }) => ({
 const mapDispatchToProps = (dispatch) => ({
   login: (user) => dispatch(login(user)),
   logout: () => dispatch(logout()),
+  receiveSearch: (input) => dispatch(receiveSearch(input))
 });
 
 export default connect(
